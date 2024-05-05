1128e72fb502fa1756595870cfabd8db02f28f12
Pasham Janani Lakshmi <73428819+janani-1901@users.noreply.github.com>
2024-05-04 23:50:00 -0400

    Update UserDashboard.js

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -2,20 +2,11 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styling/AdminDashboard.css';
 import InputCode from './InputCode';
-import ViewSummary from './ViewSummary';
 import Translator from './Translator';
-import Feedback from './styling/Feedback';
 
 function UserDashboard() {
   const [activeComponent, setActiveComponent] = useState('viewSummary');
 
-  const handleViewSummaryClick = () => {
-    setActiveComponent('viewSummary');
-    setIsClicked1(!isClicked1);
-    setIsClicked2(false);
-    setIsClicked3(false);
-    setIsClicked4(false);
-  };
 
   const handleTranslatorClick = () => {
     setActiveComponent('translator');
@@ -25,13 +16,6 @@ function UserDashboard() {
     setIsClicked4(false);
   }
 
-  const handleFeedbackClick = () => {
-    setActiveComponent('feedback');
-    setIsClicked3(!isClicked3);
-    setIsClicked1(false);
-    setIsClicked2(false);
-    setIsClicked4(false);
-  }
 
   const handleInputCodeClick = () => {
     setActiveComponent('inputCode');
@@ -70,10 +54,8 @@ function UserDashboard() {
     <div className="app">
       <div className="navigationbar">
         <div className="sub-container1 top">User Dashboard</div>
-        <div className="sub-container1 top" style={containerStyle1} onClick={handleViewSummaryClick}>View Summary</div>
+        <div className="sub-container1 bottom" style={containerStyle4} onClick={handleInputCodeClick}>Summarize</div>
         <div className="sub-container1 top" style={containerStyle2} onClick={handleTranslatorClick}>Translator</div>
-        <div className="sub-container1 top" style={containerStyle3} onClick={handleFeedbackClick}>Feedback</div>
-        <div className="sub-container1 bottom" style={containerStyle4} onClick={handleInputCodeClick}>Input Code</div>
       </div>
       <div className="container2">
         <div className="sub-container2 heading">
@@ -81,9 +63,7 @@ function UserDashboard() {
           <div>Logout</div>
         </div>
         <div className="sub-container2 renderarea">
-          {activeComponent === 'viewSummary' && <ViewSummary />}
           {activeComponent === 'translator' && <Translator />}
-          {activeComponent === 'feedback' && <Feedback />}
           {activeComponent === 'inputCode' && <InputCode />}
         </div>
       </div>
