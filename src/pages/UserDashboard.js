6a7fbba6324f82b4dd6b30e4d55be28eaf60f768
Pasham Janani Lakshmi <73428819+janani-1901@users.noreply.github.com>
2024-05-06 11:50:46 -0400

    Update UserDashboard.js

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styling/AdminDashboard.css';
 import InputCode from './InputCode';
 import Translator from './Translator';
+import SingleCode from './SingleCode';
+
 
 function UserDashboard() {
   const [activeComponent, setActiveComponent] = useState('viewSummary');
@@ -15,8 +17,9 @@ function UserDashboard() {
     setIsClicked3(false);
     setIsClicked4(false);
   }
-
-
+  const handleSingleCodeClick = () => {
+    setActiveComponent('singleCode');
+  }  
   const handleInputCodeClick = () => {
     setActiveComponent('inputCode');
     setIsClicked4(!isClicked4);
@@ -54,17 +57,16 @@ function UserDashboard() {
     <div className="app">
       <div className="navigationbar">
         <div className="sub-container1 top">User Dashboard</div>
-        <div className="sub-container1 bottom" style={containerStyle4} onClick={handleInputCodeClick}>Summarize</div>
-        <div className="sub-container1 top" style={containerStyle2} onClick={handleTranslatorClick}>Translator</div>
+        <div className="sub-container1 bottom" onClick={handleSingleCodeClick}>Input Single Code</div>
+        <div className="sub-container1 top" onClick={handleTranslatorClick}>Translator</div>
       </div>
       <div className="container2">
         <div className="sub-container2 heading">
-          <div><h4>Code Summarizer and Evaluation Tool</h4></div>
-          <div>Logout</div>
+          <h4>Code Summarizer and Evaluation Tool</h4>
         </div>
         <div className="sub-container2 renderarea">
           {activeComponent === 'translator' && <Translator />}
-          {activeComponent === 'inputCode' && <InputCode />}
+          {activeComponent === 'singleCode' && <SingleCode />}
         </div>
       </div>
     </div>
